5716155024bbb97ee0f77ce6a553e9c7dede1b7b
davemustaine <ksenofontov97@mail.ru>
2019-04-27 21:23:58 +0700

    Initial

---
+++ src/main/java/HelloWalker.java
@@ -0,0 +1,12 @@
+import antlr4.HelloBaseListener;
+import antlr4.HelloParser;
+
+public class HelloWalker extends HelloBaseListener {
+    public void enterR(HelloParser.RContext ctx ) {
+        System.out.println( "Entering R : " + ctx.ID().getText() );
+    }
+
+    public void exitR(HelloParser.RContext ctx ) {
+        System.out.println( "Exiting R" );
+    }
+}
